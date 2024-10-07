18eb7bfdd5eae7d58720aa408b3fd6705b7e1b9a
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2024-10-07 11:14:57 +0530

    Update start.py
    Start pic code added

--- plugins/start.py
+++ plugins/start.py
@@ -143,19 +143,20 @@ async def start_command(client: Client, message: Message):
                   InlineKeyboardButton('🍁 ᴊᴏɪɴ ᴍᴏʀᴇ', url='https://t.me/Uchiha_Community/40')]]
             )
 #Added form here
-    #if START_PIC:
-    #    await message.reply_photo(START_PIC, caption=START_MSG.format(
-    #            first = message.from_user.first_name,
-     #          last = message.from_user.last_name,
-      #          username = None if not message.from_user.username else '@' + message.from_user.username,
-   #             mention = message.from_user.mention,
-      #          id = message.from_user.id
-       #     ), 
-        #    reply_markup=reply_markup
-     #   ) 
-   # else:
+    if START_PIC:
+        await message.reply_photo(START_PIC, caption=START_MSG.format(
+                first = message.from_user.first_name,
+               last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ), 
+            reply_markup=reply_markup
+        ) 
+    else:
          #Above code is added for Start pic  
-            await message.reply_photo(START_PIC, caption=START_MSG.format(
+            await message.reply_text(
+                text = START_MSG.format(
                     first=message.from_user.first_name,
                     last=message.from_user.last_name,
                     username=None if not message.from_user.username else '@' + message.from_user.username,
